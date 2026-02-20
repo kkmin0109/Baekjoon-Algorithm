ac81f8b3badc1876c0f5b438e6c619fd50e43dc6
kkmin0109 <261355575+kkmin0109@users.noreply.github.com>
2026-02-20 21:55:14 +0900

    solve: 나머지 연산의 분배법칙

---
+++ 10430.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    
+    cin >> A >> B >> C;
+
+    cout << (A+B)%C << endl << ((A%C) + (B%C))%C << endl
+    << (A*B)%C << ((A%C) * (B%C))%C << endl;
+
+    return 0;
+
+}
